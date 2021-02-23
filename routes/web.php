ed2bebe98418c14041b246cca7d637287bbb717c
Luo <272757224@qq.com>
2021-02-23 10:13:51 +0000

    editor

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    Route::get('/', 'StaticPagesController@home');
+    Route::get('/help', 'StaticPagesController@help');
+    Route::get('/about', 'StaticPagesController@about');
+
 });
