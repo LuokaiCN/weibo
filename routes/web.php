fccf3056b29e5b80048208a3d9f839ddbca19076
Luo <272757224@qq.com>
2021-02-23 10:41:55 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-
-    Route::get('/', 'StaticPagesController@home');
-    Route::get('/help', 'StaticPagesController@help');
-    Route::get('/about', 'StaticPagesController@about');
-
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
